ac8ee842c0ca65f11c60615778cbea474be7587c
karen102_14 <karen102_14@hotmail.com>
2023-09-12 20:09:08 -0400

    added store.js, favorites reducers addFavorite and removeFavorite, favorites selectors and favorites slice

---
+++ src/favorites/store/favoritesSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+import reducers from './favoritesReducers'
+
+export const favoritesSlice = createSlice({
+    name: 'favorites',
+    initialState: {
+        favorites: [],
+    },
+    reducers: reducers,
+});
+
+export const { addFavorite, removeFavorite } = favoritesSlice.actions;
+
+export default favoritesSlice.reducer;
